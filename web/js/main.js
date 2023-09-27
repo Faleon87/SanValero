fa6025886b9c1bf1d10301c17abda9113a9e8cfb
Jaime <70387697+Faleon87@users.noreply.github.com>
2023-09-27 09:45:21 +0200

    Jaime
    Estructura de api

---
+++ web/js/main.js
@@ -0,0 +1,23 @@
+const apiKey = 'gEDeWAK4aqBfdsi3N9uXGFINbe2lz1ulkOGJIpDP';
+const sol = 1000; // El número de sol que deseas consultar
+
+// URL del API de la NASA
+const apiUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&api_key=${apiKey}`;
+
+// Realizar una solicitud GET utilizando Fetch API
+fetch(apiUrl)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error('Error en la solicitud al API');
+    }
+    return response.json();
+  })
+  .then((data) => {
+    // Los datos de las fotos se encuentran en el objeto 'data'
+    console.log(data);
+    // Aquí puedes procesar los datos o mostrar las imágenes en tu aplicación
+   for
+  })
+  .catch((error) => {
+    console.error('Ocurrió un error:', error);
+  });
