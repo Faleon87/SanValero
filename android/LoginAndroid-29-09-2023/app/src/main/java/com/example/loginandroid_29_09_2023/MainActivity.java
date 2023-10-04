3a548329646b62775bca310997fc0cb16838f9de
Jaime Vijuesca Fraca <70387697+Faleon87@users.noreply.github.com>
2023-10-04 10:42:52 +0200

    jaime
    keep id (Email , Password , Button)

--- android/LoginAndroid-29-09-2023/app/src/main/java/com/example/loginandroid_29_09_2023/MainActivity.java
+++ android/LoginAndroid-29-09-2023/app/src/main/java/com/example/loginandroid_29_09_2023/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.loginandroid_29_09_2023;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initComponents();
     }
+
+    private void initComponents() {
+        View edtEmail = findViewById(R.id.edtEmail);
+        View edtPassword= findViewById(R.id.edtPassword);
+        View btnLogin=findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+   }
+
 }
